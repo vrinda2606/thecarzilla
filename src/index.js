c315cdb762fefd4af9cd2201090396cb4f5c17d3
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-03-22 00:15:30 +0530

    links

--- src/index.js
+++ src/index.js
@@ -4,26 +4,12 @@ import "./index.css";
 import Navbar from "./Components/Navbar";
 import Footer from "./Components/Footer";
 import {BrowserRouter} from "react-router-dom";
-// import AboutUs from "./Components/AboutUs";
 import App from "./App";
-// import TermsCond from "./Components/TermsCond";
-// import PrivacyPolicy from "./Components/PrivacyPolicy";
-// import CustomerSupport from "./Components/CustomerSupport";
-// import TermsCondII from "./Components/TermsCondII";
-// import PrivacyPolicyII from "./Components/PrivacyPolicyII";
-// import HowDoesItWork from "./Components/HowDoesItWork";
 
 
 ReactDOM.render(
 <BrowserRouter>
 <Navbar />
 <App />
-{/* <TermsCond /> */}
-{/* <PrivacyPolicy /> */}
-{/* <CustomerSupport /> */}
-{/* <TermsCondII /> */}
-{/* <PrivacyPolicyII /> */}
-{/* <HowDoesItWork /> */}
-{/* <AboutUs /> */}
 <Footer />
 </BrowserRouter>,document.getElementById('root'));
