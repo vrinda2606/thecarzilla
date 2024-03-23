c435427751d584b31020cc332d6140e280264d91
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-03-24 00:25:31 +0530

    Lite App

--- src/Components/LiteApp.js
+++ src/Components/LiteApp.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from 'react';
 
 const LiteApp = () => {
   return (
-    <div className='WhiteBg' style={{height: "615px"}}>
-      
+    <div className='WhiteBg AppLite' style={{height: "615px"}}>
+           <div className='appLite'>
+                <div className='appLiteHeading'>
+                    <div className='headingBoxWhite'>
+                    <div className='HeadingWhiteDiv'>
+                      <span className='WhiteDivfirst'><hr/>COMING SOON<hr/></span>
+                      <span className='WhiteDivMiddle'><h1>TheCarzilla.in </h1></span>
+                      <span className='WhiteDivLast' style={{color : "#000000"}}>One stop solution app for all your car needs</span>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="buttonContact">
+                    <button className="btnContact" style={{borderRadius : '0'}}>Coming Soon</button>
+                </div>
+
+                <p className='para'>An App that caters all your car needs, Including Genuine Spare Parts, Fastag Recharge & many more</p>
+                <h6>Soon Available on</h6>
+                <div className='playStore'>
+                    <div className="playStoreBtnI"></div>
+                   <div className='playStoreBtnII'></div>
+                </div>
+           </div>
+           <div className='mobileApp' >
+                <img className='mobileAppBg' src='../Images/LiteAppBg.png' alt='' />
+                <img className='LiteAppBgPhone' src='../Images/LiteAppBgPhone.png' alt=''/>
+           </div>
     </div>
   )
 }
