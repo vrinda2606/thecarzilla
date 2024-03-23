0c528fc7b822c3d09c216d5c7b077cd5072736c0
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-03-23 18:42:03 +0530

    Team

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -3,13 +3,39 @@ import { NavLink } from 'react-router-dom';
 
 const Team = () => {
   return (
-    <div className='BlackBg' style={{height : '740px'}}>
+    <div className='BlackBg' style={{height : '780px'}}>
         <div className='HeadingBoxBlack'>
         <div className='HeadingBlackDiv'>
             <span className='BlackDivfirst'><hr/><p >OUR TEAM</p><hr/></span>
             <span className='BlackDivMiddle'><h1>Meet Our Leadership Team</h1></span>
             <span className='BlackDivLast'>A team dedicated towards their customers</span>
       </div>
+      </div>
+
+      <div className='TeamCoordinators'>
+         <div className='team-mates'>
+            <div className='team-mates-bg'></div>
+            <div className='team-mates-names'>
+               <h4>Permeet Singh</h4>
+               <p>Co-Founder, Head of Sales</p>
+            </div>
+         </div>
+         <div className='team-mates'>
+            <div className='team-mates-bg'>
+               <img className='team-mates-img' src='../Images/Team/MGimage.png' alt='mgImage' />
+            </div>
+            <div className='team-mates-names'>
+               <h4>Madhav Garg</h4>
+               <p>Founder, CEO</p>
+            </div>
+         </div>
+         <div className='team-mates'>
+            <div className='team-mates-bg'></div>
+            <div className='team-mates-names'>
+               <h4>Shubham Gupta</h4>
+               <p>Chief Marketing Officer</p>
+            </div>
+         </div>
       </div>
       
       <NavLink exact to='/AboutUs' onClick={() => { window.scrollTo({top: 0,behavior: "instant"})}}>
