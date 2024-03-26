312d92cdfb685dd0501db5a32f2e40fd09e12226
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-03-26 18:28:34 +0530

    testimonials

--- src/Components/Testimonial.js
+++ src/Components/Testimonial.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import { faQuoteRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from 'react';
+import { testimonialContent } from '../utils/testimonialsContent';
 
 const Testimonial = () => {
   return (
@@ -9,6 +12,37 @@ const Testimonial = () => {
               <span className='WhiteDivMiddle'><h1>What Customer say about us?</h1></span>
               <span className='WhiteDivLast'>Delivering happiness</span>
         </div>
+        </div>
+        
+
+        <div className='testimonialCarousel'>
+            {testimonialContent.map( (item,index) => (
+              <div key={index} className='testimonials'>
+                <div class="blackCircle"><FontAwesomeIcon icon={faQuoteRight} /></div>
+                <div className='testimonialHeader'>
+                      <div class="customerImage"><img src={item.img} alt=""/></div>
+                      <div class="customerNameReview"><p>{item.name}</p><p>{item.rating}</p></div>
+                </div>
+                <div className='testimonailReview'>
+                      <p>{item.review}</p>
+                </div>
+              </div>
+            )
+            )}
+            
+            {/*
+            <div className='testimonials'>
+                <div class="blackCircle"><FontAwesomeIcon icon={faQuoteRight} /></div>
+                <div className='testimonialHeader'>
+                      <div class="customerImage"><img src='../Images/CustomerImage.png' alt=""/></div>
+                      <div class="customerNameReview"><p>Jaspreet Singh</p><p>⭐⭐⭐⭐</p></div>
+                </div>
+                <div className='testimonailReview'>
+                      <p>"Buying my Kia Seltos from theCarzilla.in was an absolute breeze! The seamless process, coupled with their excellent customer service, made my car buying experience truly delightful. I'm loving every moment in my new ride!"</p>
+                </div>
+            </div>
+            */}
+
         </div>
     </div>
   )
--- src/utils/testimonialsContent.js
+++ src/utils/testimonialsContent.js
@@ -1,44 +1,44 @@
 export const testimonialContent = [
     {
         id : 1,
-        img : "",
+        img : "../Images/CustomerImage.png",
         name : "Jaspreet Singh",
-        rating : 4,
+        rating : '⭐⭐⭐⭐',
         review : "Buying my Kia Seltos from theCarzilla.in was an absolute breeze! The seamless process, coupled with their excellent customer service, made my car buying experience truly delightful. I'm loving every moment in my new ride!",
     },
     {
         id : 2,
         img : "",
         name : "Manjeet Singh",
-        rating : 4,
+        rating : '⭐⭐⭐⭐',
         review : "Choosing the Honda Elevate from theCarzilla.in was a decision I'll never regret. From their extensive selection to their impeccable service, theCarzilla.in made my car buying journey smooth and satisfying.",
     },
-    {
-        id : 3,
-        img : "",
-        name : "Anmol Chaudhary",
-        rating : 4,
-        review : "Getting my Tata Punch from theCarzilla.in was a fantastic choice! Their seamless process and knowledgeable staff made purchasing my dream car a breeze. I'm thrilled with my Tata Punch and couldn't be happier with theCarzilla.in's service!",
-    },
-    {
-        id : 4,
-        img : "",
-        name : "Jaspreet Singh",
-        rating : 4,
-        review : "Buying my Kia Seltos from theCarzilla.in was an absolute breeze! The seamless process, coupled with their excellent customer service, made my car buying experience truly delightful. I'm loving every moment in my new ride!",
-    },
-    {
-        id : 5,
-        img : "",
-        name : "Manjeet Singh",
-        rating : 4,
-        review : "Choosing the Honda Elevate from theCarzilla.in was a decision I'll never regret. From their extensive selection to their impeccable service, theCarzilla.in made my car buying journey smooth and satisfying.",
-    },
-    {
-        id : 6,
-        img : "",
-        name : "Anmol Chaudhary",
-        rating : 4,
-        review : "Getting my Tata Punch from theCarzilla.in was a fantastic choice! Their seamless process and knowledgeable staff made purchasing my dream car a breeze. I'm thrilled with my Tata Punch and couldn't be happier with theCarzilla.in's service!",
-    }
+    // {
+    //     id : 3,
+    //     img : "",
+    //     name : "Anmol Chaudhary",
+    //     rating : '⭐⭐⭐⭐',
+    //     review : "Getting my Tata Punch from theCarzilla.in was a fantastic choice! Their seamless process and knowledgeable staff made purchasing my dream car a breeze. I'm thrilled with my Tata Punch and couldn't be happier with theCarzilla.in's service!",
+    // },
+    // {
+    //     id : 4,
+    //     img : "",
+    //     name : "Jaspreet Singh",
+    //     rating : '⭐⭐⭐⭐',
+    //     review : "Buying my Kia Seltos from theCarzilla.in was an absolute breeze! The seamless process, coupled with their excellent customer service, made my car buying experience truly delightful. I'm loving every moment in my new ride!",
+    // },
+    // {
+    //     id : 5,
+    //     img : "",
+    //     name : "Manjeet Singh",
+    //     rating : '⭐⭐⭐⭐',
+    //     review : "Choosing the Honda Elevate from theCarzilla.in was a decision I'll never regret. From their extensive selection to their impeccable service, theCarzilla.in made my car buying journey smooth and satisfying.",
+    // },
+    // {
+    //     id : 6,
+    //     img : "",
+    //     name : "Anmol Chaudhary",
+    //     rating : '⭐⭐⭐⭐',
+    //     review : "Getting my Tata Punch from theCarzilla.in was a fantastic choice! Their seamless process and knowledgeable staff made purchasing my dream car a breeze. I'm thrilled with my Tata Punch and couldn't be happier with theCarzilla.in's service!",
+    // }
 ];
