7ebff1cee80b25e7fbb43f51ab56341d604d7785
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-04-10 16:09:43 +0530

    popup2 updated

--- src/Components/CustomerSupport.js
+++ src/Components/CustomerSupport.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPhoneVolume , faEnvelopeOpenText , faCircleXmark} from '@fortawesome/free-solid-svg-icons';
-import React, { useState } from 'react';
+import { faPhoneVolume , faEnvelopeOpenText,faCircleXmark} from '@fortawesome/free-solid-svg-icons';
+import React,{useState} from 'react';
 import {images} from '../utils/constants';
 
 const CustomerSupport = () => {
@@ -12,8 +12,9 @@ const CustomerSupport = () => {
   const nameInput = document.getElementById('getInTouchName').value.trim();
   const emailInput = document.getElementById('getInTouchEmail').value.trim();
   const numberInput = document.getElementById('getInTouchNumber').value.trim();
+  const choiceInput = document.getElementById('howDidYouFindUs').value.trim();
 
-  if (nameInput && emailInput && numberInput) {
+  if (nameInput && emailInput && numberInput && choiceInput) {
     e.preventDefault();
     setPopupVis(true);
   } else {
@@ -23,7 +24,6 @@ const CustomerSupport = () => {
   const closePopup = () => {
     setPopupVis(false);
   };
-
 
   return (
     <>
@@ -55,7 +55,8 @@ const CustomerSupport = () => {
             
         </div>
 
-        <div className='getInTouch'>
+        <div id='getInTouchId' className='getInTouch'>
+        
           <div className={`getInTouchDiv ${popupVis ? 'active' : ''}`}>
             <div className='getInTouchForm'>
                <div className='TouchFormHeading'>
@@ -63,18 +64,18 @@ const CustomerSupport = () => {
                   <p className='TouchFormHeadingHead'>Unlocking endless support for your car buying needs</p>
                </div>
 
-               <form className='getInTouchFormDetails'>
-                  <input id='getInTouchName' type='text' placeholder='Name *'></input>
-                  <input id='getInTouchEmail' type='email' placeholder='Email'></input>
-                  <input id='getInTouchNumber' type='number' placeholder='Phone number *'></input>
-                  <select>
-                      <option value="default" selected>How did you find us?</option>
+               <form id='getInTouchFormDetailsID' className='getInTouchFormDetails'>
+                  <input id='getInTouchName' type='text' placeholder='Name *' required />
+                  <input id='getInTouchEmail' type='email' placeholder='Email' required />
+                  <input id='getInTouchNumber' type='number' placeholder='Phone number *' required />
+                  <select id="howDidYouFindUs" required>
+                      <option value="" selected>How did you find us?</option>
                       <option value="Instagram">Instagram</option>
-                      <option value="linkedIn">linkedIn</option>
+                      <option value="LinkedIn">LinkedIn</option>
                       <option value="Twitter">Twitter</option>
                       <option value="Others">Others</option>
                   </select>
-                  <button onClick={openPopup}>SEND</button>
+                  <button type='submit' className="btnContactGIT" onClick={openPopup}>SEND</button>
                </form>
 
                <div className='getInTouchFormContact'>
@@ -92,9 +93,9 @@ const CustomerSupport = () => {
             <div className='getInTouchPic'></div>
           </div>
 
-          <div className={`BlackBg popup ${popupVis ? 'active' : ''}`}>
-              <span className='popupCloseDiv'>
-                 <button id='close_popup' onClick={closePopup}><FontAwesomeIcon className='popupCloseDivIcon' icon={faCircleXmark}></FontAwesomeIcon></button>
+          <div className={`BlackBg popup2 ${popupVis ? 'active' : ''}`}>
+              <span className='popupCloseDiv2'>
+                 <button id='close_popup2' onClick={closePopup}><FontAwesomeIcon className='popupCloseDivIcon2' icon={faCircleXmark}></FontAwesomeIcon></button>
               </span>
               <div className='HeadingBoxBlack'>
               <div className='HeadingBlackDiv'>
@@ -105,6 +106,8 @@ const CustomerSupport = () => {
               </div>
               </div>
           </div>
+
+          
         </div>
     </>
   )
