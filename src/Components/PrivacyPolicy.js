47b8f9d2ef4c83aea3f4e9a3c34aa100cd703bbb
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-04-13 22:46:17 +0530

    Privacy policy Responsiveness

--- src/Components/PrivacyPolicy.js
+++ src/Components/PrivacyPolicy.js
@@ -5,10 +5,10 @@ import { images } from '../utils/constants';
 const PrivacyPolicy = () => {
   return (
     <>
-        <div style={{backgroundImage: `url(${images[22].url})`,backgroundSize: "cover",backgroundRepeat : "no-repeat", width: "100%",height: "600px"}}>
+        <div style={{backgroundImage: `url(${images[22].url})`,backgroundSize: "cover",backgroundRepeat : "no-repeat", width: "100%",height: "47vw"}}>
         </div>
 
-        <div className='BlackBg' style={{height : '500px'}}>
+        <div className='BlackBg' style={{height : '33vw'}}>
             
         <div className='HeadingBoxBlack'>
         <div className='HeadingBlackDiv'>
@@ -18,13 +18,15 @@ const PrivacyPolicy = () => {
         </div>
         </div>
         
-        <div className='DivContent' style={{height : '120px'}}>
+        <div className='DivContent' style={{height : '8vw'}}>
         We at IgniteX Solutions Private Limited are committed to serving you better and value you as our customer/user of our website. We respect your right to privacy. This privacy policy directs the way in which TheCarzilla.in collects, uses, maintains and discloses information collected from you in the course of using the website <a href='https://thecarzilla.in/' style={{color : 'yellow'}}> thecarzilla.in</a>. This privacy policy applies to the website of TheCarzilla.in and all the services offered by TheCarzilla.in.
         </div>
             
+          <NavLink exact to='/PrivacyPolicy/:PrivacyPolicyII' onClick={() => { window.scrollTo({top: 0,behavior: "instant"})}}>
             <div className='btnForMoreDiv' >
-            <NavLink exact to='/PrivacyPolicy/:PrivacyPolicyII' onClick={() => { window.scrollTo({top: 0,behavior: "instant"})}}><button className='btnForMore'>Learn More</button></NavLink>
+            <button className='btnForMore'>Learn More</button>
             </div>
+          </NavLink>
 
             <div className='WhiteHr'/>
             
