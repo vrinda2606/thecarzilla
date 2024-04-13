1e75ac0f0ed190eb83645fe9d24d6b3024bec1cd
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-04-13 23:53:41 +0530

    Responsiveness

--- src/Components/CustomerSupport.js
+++ src/Components/CustomerSupport.js
@@ -27,10 +27,10 @@ const CustomerSupport = () => {
 
   return (
     <>
-        <div style={{backgroundImage: `url(${images[19].url})`,backgroundSize: "cover",backgroundRepeat : "no-repeat", width: "100%",height: "690px"}}>
+        <div style={{backgroundImage: `url(${images[19].url})`,backgroundSize: "cover",backgroundRepeat : "no-repeat", width: "100%",height: "47vw"}}>
         </div>
 
-        <div className='BlackBg' style={{height : '588px'}}>
+        <div className='BlackBg' style={{height : '40vw'}}>
             
         <div className='HeadingBoxBlack'>
         <div className='HeadingBlackDiv'>
@@ -40,7 +40,7 @@ const CustomerSupport = () => {
         </div>
         </div>
         
-        <div className='DivContent' style={{height : '210px'}}>
+        <div className='DivContent' style={{height : '13.5vw'}}>
         TheCarzilla.in is the beginning of a new era. One that is built on the foundation of transparent and meaningful automotive content in India. At TheCarzilla.in we will bring you the latest news, reviews, tech content and more to help you get closer to your buying decision.
         <br />
         TheCarzilla.in also promises to give the best prices and fastest delivery of your dream new car. 
--- src/Components/TermsCond.js
+++ src/Components/TermsCond.js
@@ -5,10 +5,10 @@ import { images } from '../utils/constants';
 const TermsCond = () => {
   return (
     <>
-       <div style={{backgroundImage: `url(${images[21].url})`,backgroundSize: "cover",backgroundRepeat : "no-repeat", width: "100%",height: "590px"}}>
+       <div style={{backgroundImage: `url(${images[21].url})`,backgroundSize: "cover",backgroundRepeat : "no-repeat", width: "100%",height: "40vw"}}>
         </div>
 
-        <div className='BlackBg' style={{height : '567px'}}>
+        <div className='BlackBg' style={{height : '42vw'}}>
             
         <div className='HeadingBoxBlack'>
         <div className='HeadingBlackDiv'>
@@ -18,13 +18,14 @@ const TermsCond = () => {
         </div>
         </div>
         
-        <div className='DivContent' style={{height : '210px'}}>
+        <div className='DivContent' style={{height : '17vw'}}>
            IgniteX Solutions Private Limited operates a market place platform under the brand name, TheCarzilla.in (Platform) for the purpose of facilitating the sale and purchase of new vehicles between third party vendors and the customers of the Platform. Although you may be able to conduct payment and other transactions through the Platform using third-party vendors, the Company is not in any way involved in such transactions. As a result, you hereby agree, understand and acknowledge that TheCarzilla.in is only a facilitator and cannot be a party to control in any manner any transactions on the Website. Accordingly, the contract of purchase of products on the website shall strictly be a bipartite contract between you and the sellers on the Website.
         </div>
-            
+         <NavLink exact to='/TermsAndConditions/:TermsAndConditionsII' onClick={() => { window.scrollTo({top: 0,behavior: "instant"})}}>
             <div className='btnForMoreDiv' >
-            <NavLink exact to='/TermsAndConditions/:TermsAndConditionsII' onClick={() => { window.scrollTo({top: 0,behavior: "instant"})}}><button className='btnForMore'>Learn More</button></NavLink>
+            <button className='btnForMore'>Learn More</button>
             </div>
+         </NavLink>
 
             <div className='WhiteHr'/>
             
