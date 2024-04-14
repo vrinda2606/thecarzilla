4aef37609ae423672772474d052338e96a14bd3a
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-04-14 21:45:36 +0530

    About Us Responsiveness

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -43,81 +43,66 @@ const AboutUs = () => {
           </div>
         </div>
 
-
-      <div>
-      <div style={{textAlign:"center",maxWidth: "942px",maxHeight: "210px",display:"flex",flexDirection:"row",justifyContent: "space-between",margin: "70px auto"}}>
-         <div className='card' style={{width: "280px",height: "209px",display: 'flex',flexDirection: "column",alignItems: "center",justifyContent: "space-around"}}>
-           <div className='cardIcon' style={{backgroundColor: "#000000",width: "64px",height:"64px",color:"white",display: "flex", justifyContent: "center", alignItems: "center" }}>
-           <FontAwesomeIcon icon={faLayerGroup} style={{ color: "white",width: "25px",height:"25px"}} />
+      <div className='AboutUs'>
+         <div className='cardAboutUs'>
+           <div className='cardIconAboutUs'>
+           <FontAwesomeIcon icon={faLayerGroup} className='cardIconLogoAboutUs'/>
            </div>
-           <div className='cardHeading' style={{width: "auto",height:"25px"}}><h6 style={{height:"100%",width:"100%",fontWeight: "600",fontSize: "18px",fontFamily: "Lexend Deca", lineHeight: "25.2px",color: "#03040A"}}>Best Price</h6></div>
-           <div className='cardDesc' style={{width:" 280px",height:"80px"}}>
-              <p style={{fontFamily: "Space Grotesk",fontWeight: "400",fontSize: "15px",lineHeight: "25px",color: '#7F808E'}}>
-              We offer You the lowest price in the market.
-              </p>
+           <div className='cardHeadingAboutUs'><h6>Best Price</h6></div>
+           <div className='cardDescAboutUs'>
+              <p>We offer You the lowest price in the market.</p>
            </div>
          </div>
 
-         <div className='card' style={{width: "280px",height: "209px",display: 'flex',flexDirection: "column",alignItems: "center",justifyContent: "space-around"}}>
-           <div className='cardIcon' style={{backgroundColor: "#000000",width: "64px",height:"64px",color:"white",display: "flex", justifyContent: "center", alignItems: "center" }}>
-           <FontAwesomeIcon icon={faLock} style={{ color: "white",width: "25px",height:"25px"}} />
+         <div className='cardAboutUs'>
+           <div className='cardIconAboutUs'>
+           <FontAwesomeIcon icon={faLock} className='cardIconLogoAboutUs'/>
            </div>
-           <div className='cardHeading' style={{width: "auto",height:"25px"}}><h6 style={{height:"100%",width:"100%",fontWeight: "600",fontSize: "18px",fontFamily: "Lexend Deca", lineHeight: "25.2px",color: "#03040A"}}>What Drives Us?</h6></div>
-           <div className='cardDesc' style={{width:" 280px",height:"80px"}}>
-              <p style={{fontFamily: "Space Grotesk",fontWeight: "400",fontSize: "15px",lineHeight: "25px",color: '#7F808E'}}>
-              A Determination to make your car purchase journey,a smooth ride.
-              </p>
+           <div className='cardHeadingAboutUs'><h6>What Drives Us?</h6></div>
+           <div className='cardDescAboutUs'>
+              <p>A Determination to make your car purchase journey,a smooth ride.</p>
            </div>
          </div>
-         <div className='card' style={{width: "280px",height: "209px",display: 'flex',flexDirection: "column",alignItems: "center",justifyContent: "space-around"}}>
-           <div className='cardIcon' style={{backgroundColor: "#000000",width: "64px",height:"64px",color:"white",display: "flex", justifyContent: "center", alignItems: "center" }}>
-           <FontAwesomeIcon icon={faHeadphones} style={{ color: "white",width: "25px",height:"25px"}} />
+         <div className='cardAboutUs'>
+           <div className='cardIconAboutUs'>
+           <FontAwesomeIcon icon={faHeadphones} className='cardIconLogoAboutUs'/>
            </div>
-           <div className='cardHeading' style={{width: "auto",height:"25px"}}><h6 style={{height:"100%",width:"100%",fontWeight: "600",fontSize: "18px",fontFamily: "Lexend Deca", lineHeight: "25.2px",color: "#03040A"}}>Earliest Available</h6></div>
-           <div className='cardDesc' style={{width:" 280px",height:"80px"}}>
-              <p style={{fontFamily: "Space Grotesk",fontWeight: "400",fontSize: "15px",lineHeight: "25px",color: '#7F808E'}}>
-              We Promise to get you your dream car before anyone else in the market.
-              </p>
+           <div className='cardHeadingAboutUs'><h6>Earliest Available</h6></div>
+           <div className='cardDescAboutUs'>
+              <p>We Promise to get you your dream car before anyone else in the market.</p>
            </div>
          </div>
       </div>
 
-      <div style={{textAlign:"center",maxWidth: "942px",maxHeight: "210px",display:"flex",flexDirection:"row",justifyContent: "space-between",margin: "70px auto"}}>
-         <div className='card' style={{width: "280px",height: "209px",display: 'flex',flexDirection: "column",alignItems: "center",justifyContent: "space-around"}}>
-           <div className='cardIcon' style={{backgroundColor: "#000000",width: "64px",height:"64px",color:"white",display: "flex", justifyContent: "center", alignItems: "center" }}>
-           <FontAwesomeIcon icon={faLayerGroup} style={{ color: "white",width: "25px",height:"25px"}} />
+      <div className='AboutUs'>
+         <div className='cardAboutUs'>
+           <div className='cardIconAboutUs'>
+           <FontAwesomeIcon className='cardIconLogoAboutUs' icon={faLayerGroup}/>
            </div>
-           <div className='cardHeading' style={{width: "auto",height:"25px"}}><h6 style={{height:"100%",width:"100%",fontWeight: "600",fontSize: "18px",fontFamily: "Lexend Deca", lineHeight: "25.2px",color: "#03040A"}}>Door-Step Delivery</h6></div>
-           <div className='cardDesc' style={{width:" 280px",height:"80px"}}>
-              <p style={{fontFamily: "Space Grotesk",fontWeight: "400",fontSize: "15px",lineHeight: "25px",color: '#7F808E'}}>
-              Financial assistance, Used car inspection, payment and delivery - all done at your doorstep.
-              </p>
+           <div className='cardHeadingAboutUs'><h6>Door-Step Delivery</h6></div>
+           <div className='cardDescAboutUs'>
+              <p>Financial assistance, Used car inspection, payment and delivery - all done at your doorstep.</p>
            </div>
          </div>
 
-         <div className='card' style={{width: "280px",height: "209px",display: 'flex',flexDirection: "column",alignItems: "center",justifyContent: "space-around"}}>
-           <div className='cardIcon' style={{backgroundColor: "#000000",width: "64px",height:"64px",color:"white",display: "flex", justifyContent: "center", alignItems: "center" }}>
-           <FontAwesomeIcon icon={faLock} style={{ color: "white",width: "25px",height:"25px"}} />
+         <div className='cardAboutUs'>
+           <div className='cardIconAboutUs'>
+           <FontAwesomeIcon className='cardIconLogoAboutUs' icon={faLock}/>
            </div>
-           <div className='cardHeading' style={{width: "auto",height:"25px"}}><h6 style={{height:"100%",width:"100%",fontWeight: "600",fontSize: "18px",fontFamily: "Lexend Deca", lineHeight: "25.2px",color: "#03040A"}}>What Fuels Us?</h6></div>
-           <div className='cardDesc' style={{width:" 280px",height:"80px"}}>
-              <p style={{fontFamily: "Space Grotesk",fontWeight: "400",fontSize: "15px",lineHeight: "25px",color: '#7F808E'}}>
-              We are a group of ex co-founders, engineers and top of all- car lovers.
-              </p>
+           <div className='cardHeadingAboutUs'><h6>What Fuels Us?</h6></div>
+           <div className='cardDescAboutUs'>
+              <p>We are a group of ex co-founders, engineers and top of all- car lovers.</p>
            </div>
          </div>
-         <div className='card' style={{width: "280px",height: "209px",display: 'flex',flexDirection: "column",alignItems: "center",justifyContent: "space-around"}}>
-           <div className='cardIcon' style={{backgroundColor: "#000000",width: "64px",height:"64px",color:"white",display: "flex", justifyContent: "center", alignItems: "center" }}>
-           <FontAwesomeIcon icon={faHeadphones} style={{ color: "white",width: "25px",height:"25px"}} />
+         <div className='cardAboutUs'>
+           <div className='cardIconAboutUs'>
+           <FontAwesomeIcon className='cardIconLogoAboutUs' icon={faHeadphones}/>
            </div>
-           <div className='cardHeading' style={{width: "auto",height:"25px"}}><h6 style={{height:"100%",width:"100%",fontWeight: "600",fontSize: "18px",fontFamily: "Lexend Deca", lineHeight: "25.2px",color: "#03040A"}}>Complete Transparency</h6></div>
-           <div className='cardDesc' style={{width:" 280px",height:"80px"}}>
-              <p style={{fontFamily: "Space Grotesk",fontWeight: "400",fontSize: "15px",lineHeight: "25px",color: '#7F808E'}}>
-              We offer best price guarantee and will never send any hidden charges your way.
-              </p>
+           <div className='cardHeadingAboutUs'><h6>Complete Transparency</h6></div>
+           <div className='cardDescAboutUs'>
+              <p>We offer best price guarantee and will never send any hidden charges your way.</p>
            </div>
          </div>
-      </div>
       </div>
        </div>
     </div>
