6132d686d042be4c8dd92152c7164909eff57122
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-11-13 01:19:58 +0530

    Find my car section

--- src/Components/FindCar.jsx
+++ src/Components/FindCar.jsx
@@ -10,15 +10,15 @@ const FindCar = () => {
         <p className="mobile-find-car-description">Answer a few questions to find a car that fits your needs</p>
         <button className="mobile-find-car-button">Find my car</button>
       </div>
-      <div className="mobile-card-container">
-        <div className="mobile-card mobile-card-green">
+      <div className="mobile-card-row">
+        <div className="mobile-card mobile-card-orange">
           <img src={newCardPhoto} alt="Car" className="mobile-card-image" />
         </div>
         <div className="mobile-card mobile-card-white">
           <img src={newCardPhoto} alt="Car" className="mobile-card-image" />
           <img src={checkIcon} alt="Check" className="mobile-card-check-icon" />
         </div>
-        <div className="mobile-card mobile-card-orange">
+        <div className="mobile-card mobile-card-green">
           <img src={newCardPhoto} alt="Car" className="mobile-card-image" />
         </div>
       </div>
