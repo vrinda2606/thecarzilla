e0611029ed151386b01b84a07eff80ed5dc098db
AtharvaSachan123 <atharvasachan823@gmail.com>
2024-11-16 22:15:00 +0530

    Added popup to the master page button

--- src/Components/Master.jsx
+++ src/Components/Master.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import Background from "./Images/mobile version/background.png"
-import Car from "./Images/mobile version/3d-car.png"
-import Salesman from "./Images/mobile version/saleman.png"
+import React, { useState } from 'react'
+import Background from "./Images/mobile version/futuristic-interior-sci-fi-room-background 2.png"
+import Car from "./Images/mobile version/view-3d-car-removebg-preview 1 (1).png"
+import Salesman from "./Images/mobile version/smiling-handsome-hispanic-salesman-handing-keys-new-car-studio 1 (1).png"
+import PopUp from './PopUp'
 
 const Master = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  const openPopup = () => {
+    setIsPopupOpen(true);
+  };
+
+  const closePopup = () => {
+    setIsPopupOpen(false);
+  };
+
   return (
     <div className="mobile-container-wrapper">
       <div className="mobile-ad-container">
@@ -16,12 +27,19 @@ const Master = () => {
             <div className='mobile-description-container'>
               <p className="mobile-description">Schedule a test drive from the wide range of our car collection</p>
             </div>
-            <button className="mobile-enquire-btn">Enquire Now</button>
+            <button className="mobile-enquire-btn1" onClick={openPopup}>
+              Enquire Now
+            </button>
           </div>
         </div>
         <img src={Car} alt="car" className="mobile-car-image" />
         <img src={Salesman} alt="salesman" className="mobile-salesman-image" />
       </div>
+
+      <PopUp 
+        isOpen={isPopupOpen} 
+        onClose={closePopup}
+      />
     </div>
   )
 }
