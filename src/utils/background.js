f2591f28627ebd881ce3b9e376831e9d112e4aa1
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-12-31 21:38:56 +0530

    change 2

--- src/utils/background.js
+++ src/utils/background.js
@@ -1,6 +1,5 @@
 import img1 from '../Components/Images/Hero Section/HERO SECTION IMAGE 1.png';
 import img2 from '../Components/Images/Hero Section/HERO SECTION IMAGE 2.png';
-import img3 from '../Components/Images/Hero Section/HERO SECTION IMAGE 3.png';
 export const background = [
     {
         id:1,
@@ -9,9 +8,5 @@ export const background = [
     {
         id:2,
         url:img2
-    },
-    {
-        id:3,
-        url:img3
     }
 ]
