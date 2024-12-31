a36c667aef32f460e1982a657d56aa3284164606
Vrinda Sharma <vrinda26sharma@gmail.com>
2024-12-31 21:35:41 +0530

    changes

--- src/utils/background.js
+++ src/utils/background.js
@@ -1,6 +1,6 @@
 import img1 from '../Components/Images/Hero Section/HERO SECTION IMAGE 1.png';
 import img2 from '../Components/Images/Hero Section/HERO SECTION IMAGE 2.png';
-// import img3 from '../Components/Images/Hero Section/HERO SECTION IMAGE 3.png';
+import img3 from '../Components/Images/Hero Section/HERO SECTION IMAGE 3.png';
 
 
 export const background = [
@@ -12,8 +12,8 @@ export const background = [
         id:2,
         url:img2
     },
-    // {
-    //     id:3,
-    //     url:img3
-    // }
+    {
+        id:3,
+        url:img3
+    }
 ]
